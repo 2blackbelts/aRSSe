4dcad8fb2bd70a6be8f8733e9df341e78e652107
Saim McIntosh <s.mcintosh00@gmail.com>
2017-05-21 11:26:18 +0930

    Added ShowRSS and ssh backend

--- includes/settings.php
+++ includes/settings.php
@@ -3,10 +3,26 @@
 
 $OMDB_API_KEY = '';
 
+// Your web server will need PHP's SSH2 extention
+// Installation details: http://php.net/manual/en/ssh2.installation.php
+
 // SSH login details
-$SSH_USERNAME = '';
-$SSH_PASSWORD = '';
-$SSH_PORT = '22'; //default 22
-$SSH_IP = ''; 
+$SSH_USERNAME = 'saim';
+$SSH_PASSWORD = 'unity786';
+$SSH_PORT = 22; //default 22
+$SSH_IP = '192.168.2.15';  //192.168.1.20
+
+// Attempt connection
+try {
+
+	$connection = ssh2_connect($SSH_IP, $SSH_PORT);
+	ssh2_auth_password($connection, $SSH_USERNAME, $SSH_PASSWORD);
+
+} catch (Exception $e) {
+
+	echo 'Cannot connect to SSH';
+
+}
+
 
 
